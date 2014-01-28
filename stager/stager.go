09e1a041b53d40ced3e507b73619fd0a4a4f245f
David Varvel and Ruth Byers <dvarvel@pivotallabs.com>
2014-01-28 15:41:22 -0800

    Stager now puts something in etcd
    [#63918068]

--- stager/stager.go
+++ stager/stager.go
@@ -1,15 +1,28 @@
 package stager
 
+import (
+	"github.com/cloudfoundry-incubator/runtime-schema/bbs"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+	"strings"
+)
+
 type Stager interface {
 	Stage(StagingRequest) error
 }
 
-type stager struct{}
+type stager struct {
+	stagerBBS bbs.StagerBBS
+}
 
-func NewStager() Stager {
-	return &stager{}
+func NewStager(stagerBBS bbs.StagerBBS) Stager {
+	return &stager{
+		stagerBBS: stagerBBS,
+	}
 }
 
 func (stager *stager) Stage(request StagingRequest) error {
-	return nil
+	err := stager.stagerBBS.DesireRunOnce(models.RunOnce{
+		Guid: strings.Join([]string{request.AppId, request.TaskId}, "-"),
+	})
+	return err
 }
---
+++ stager/stager_test.go
@@ -0,0 +1,32 @@
+package stager_test
+
+import (
+	"github.com/cloudfoundry-incubator/runtime-schema/bbs"
+	"github.com/cloudfoundry/storeadapter/fakestoreadapter"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/pivotal-cf-experimental/stager/stager"
+)
+
+var _ = Describe("Stage", func() {
+	var stager Stager
+	var fauxStoreAdapter *fakestoreadapter.FakeStoreAdapter
+
+	BeforeEach(func() {
+		fauxStoreAdapter = fakestoreadapter.New()
+		stagerBBS := bbs.New(fauxStoreAdapter)
+		stager = NewStager(stagerBBS)
+	})
+
+	It("puts a job in etcd", func(done Done) {
+		executorBBS := bbs.New(fauxStoreAdapter).ExecutorBBS
+		modelChannel, _, _ := executorBBS.WatchForDesiredRunOnce()
+		stager.Stage(StagingRequest{
+			AppId:  "bunny",
+			TaskId: "hop",
+		})
+
+		Ω((<-modelChannel).Guid).To(Equal("bunny-hop"))
+		close(done)
+	}, 2)
+})
