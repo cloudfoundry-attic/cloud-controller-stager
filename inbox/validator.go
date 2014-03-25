db35b536014827fa706608b9c64af26b6677b658
Alex Suraci <asuraci@pivotallabs.com>
2014-03-25 11:23:53 -0700

    validate presence of app bits download uri
    Signed-off-by: Phan Le <ple@pivotallabs.com>

--- inbox/validator.go
+++ inbox/validator.go
@@ -8,6 +8,7 @@ import (
 
 var ErrMissingAppId = errors.New("missing app id")
 var ErrMissingTaskId = errors.New("missing task id")
+var ErrMissingAppBitsDownloadUri = errors.New("missing app bits download uri")
 
 func ValidateRequest(stagingRequest models.StagingRequestFromCC) error {
 	if len(stagingRequest.AppId) == 0 {
@@ -17,5 +18,10 @@ func ValidateRequest(stagingRequest models.StagingRequestFromCC) error {
 	if len(stagingRequest.TaskId) == 0 {
 		return ErrMissingTaskId
 	}
+
+	if len(stagingRequest.AppBitsDownloadUri) == 0 {
+		return ErrMissingAppBitsDownloadUri
+	}
+
 	return nil
 }
--- inbox/validator_test.go
+++ inbox/validator_test.go
@@ -9,37 +9,43 @@ import (
 )
 
 var _ = Describe("Validator", func() {
+	var request models.StagingRequestFromCC
 	var validator RequestValidator
 
 	BeforeEach(func() {
-		validator = ValidateRequest
-	})
-
-	It("returns an error for a missing app id", func() {
-		err := validator(models.StagingRequestFromCC{
-			AppId:              "",
+		request = models.StagingRequestFromCC{
+			AppId:              "hip",
 			TaskId:             "hop",
 			AppBitsDownloadUri: "http://example-uri.com/bunny",
 			Stack:              "rabbit_hole",
 			MemoryMB:           256,
 			DiskMB:             1024,
-		})
+		}
 
+		validator = ValidateRequest
+	})
+
+	It("returns an error for a missing app id", func() {
+		request.AppId = ""
+
+		err := validator(request)
 		Ω(err).Should(HaveOccurred())
 		Ω(err.Error()).Should(Equal("missing app id"))
 	})
 
 	It("returns an error for a missing task id", func() {
-		err := validator(models.StagingRequestFromCC{
-			AppId:              "hip",
-			TaskId:             "",
-			AppBitsDownloadUri: "http://example-uri.com/bunny",
-			Stack:              "rabbit_hole",
-			MemoryMB:           256,
-			DiskMB:             1024,
-		})
+		request.TaskId = ""
 
+		err := validator(request)
 		Ω(err).Should(HaveOccurred())
 		Ω(err.Error()).Should(Equal("missing task id"))
+	})
+
+	It("returns an error for a missing download uri", func() {
+		request.AppBitsDownloadUri = ""
+
+		err := validator(request)
+		Ω(err).Should(HaveOccurred())
+		Ω(err.Error()).Should(Equal("missing app bits download uri"))
 	})
 })
