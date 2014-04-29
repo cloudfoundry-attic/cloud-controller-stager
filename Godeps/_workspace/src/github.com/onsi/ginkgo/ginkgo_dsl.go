7e3a00cc48d1f6be7d6669567f2e6940d0f529bd
Alex Suraci <asuraci@pivotallabs.com>
2014-04-29 15:10:48 -0700

    bump deps
    Signed-off-by: Momchil Atanasov <m.momchil@gmail.com>

--- Godeps/_workspace/src/github.com/onsi/ginkgo/ginkgo_dsl.go
+++ Godeps/_workspace/src/github.com/onsi/ginkgo/ginkgo_dsl.go
@@ -13,12 +13,6 @@ package ginkgo
 
 import (
 	"flag"
-	"io"
-	"net/http"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/onsi/ginkgo/config"
 	"github.com/onsi/ginkgo/internal/codelocation"
 	"github.com/onsi/ginkgo/internal/failer"
@@ -29,6 +23,11 @@ import (
 	"github.com/onsi/ginkgo/reporters"
 	"github.com/onsi/ginkgo/reporters/stenographer"
 	"github.com/onsi/ginkgo/types"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const GINKGO_VERSION = config.VERSION
@@ -66,12 +65,6 @@ var GinkgoWriter io.Writer
 //The interface by which Ginkgo receives *testing.T
 type GinkgoTestingT interface {
 	Fail()
-}
-
-//GinkgoParallelNode returns the parallel node number for the current ginkgo process
-//The node number is 1-indexed
-func GinkgoParallelNode() int {
-	return config.GinkgoConfig.ParallelNode
 }
 
 //Some matcher libraries or legacy codebases require a *testing.T
