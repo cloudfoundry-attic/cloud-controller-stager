450601eb90d7fe0084529f69fad53834d586697e
Sheel Choksi <sheelc@pivotallabs.com>
2014-07-24 09:46:40 -0700

    bump deps, update yagnats interface
    Signed-off-by: Simon Leung <leungs@us.ibm.com>

--- main.go
+++ main.go
@@ -143,7 +143,11 @@ func initializeNatsClient(logger lager.Logger) *yagnats.Client {
 	for _, addr := range strings.Split(*natsAddresses, ",") {
 		natsMembers = append(
 			natsMembers,
-			&yagnats.ConnectionInfo{addr, *natsUsername, *natsPassword},
+			&yagnats.ConnectionInfo{
+				Addr:     addr,
+				Username: *natsUsername,
+				Password: *natsPassword,
+			},
 		)
 	}
 
