6b1819e7dfe7517b9ce9c2e28b0cbe2979b94850
Tim Hausler <thausler@pivotal.io>
2016-11-23 14:41:14 -0800

    stager takes config via json
    [#134608685]
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- cmd/stager/main.go
+++ cmd/stager/main.go
@@ -16,7 +16,6 @@ import (
 	"github.com/tedsuo/ifrit/sigmon"
 
 	"code.cloudfoundry.org/bbs"
-	"code.cloudfoundry.org/cflager"
 	"code.cloudfoundry.org/clock"
 	"code.cloudfoundry.org/consuladapter"
 	"code.cloudfoundry.org/debugserver"
@@ -26,122 +25,15 @@ import (
 	"code.cloudfoundry.org/runtimeschema/cc_messages/flags"
 	"code.cloudfoundry.org/stager/backend"
 	"code.cloudfoundry.org/stager/cc_client"
+	"code.cloudfoundry.org/stager/config"
 	"code.cloudfoundry.org/stager/handlers"
 	"code.cloudfoundry.org/stager/vars"
 )
 
-var ccBaseURL = flag.String(
-	"ccBaseURL",
+var configPath = flag.String(
+	"configPath",
 	"",
-	"URI to acccess the Cloud Controller",
-)
-
-var ccUsername = flag.String(
-	"ccUsername",
-	"",
-	"Basic auth username for CC internal API",
-)
-
-var ccPassword = flag.String(
-	"ccPassword",
-	"",
-	"Basic auth password for CC internal API",
-)
-
-var privilegedContainers = flag.Bool(
-	"privilegedContainers",
-	false,
-	"Whether or not to use privileged containers for  buildpack based LRPs and tasks. Containers with a docker-image-based rootfs will continue to always be unprivileged and cannot be changed.",
-)
-
-var skipCertVerify = flag.Bool(
-	"skipCertVerify",
-	false,
-	"skip SSL certificate verification",
-)
-
-var dropsondePort = flag.Int(
-	"dropsondePort",
-	3457,
-	"port the local metron agent is listening on",
-)
-
-var bbsAddress = flag.String(
-	"bbsAddress",
-	"",
-	"Address to the BBS Server",
-)
-
-var listenAddress = flag.String(
-	"listenAddress",
-	"",
-	"Address from which the Stager serves requests",
-)
-
-var stagingTaskCallbackURL = flag.String(
-	"stagingTaskCallbackURL",
-	"",
-	"URL for staging task callbacks",
-)
-
-var fileServerURL = flag.String(
-	"fileServerURL",
-	"",
-	"URL of the file server",
-)
-
-var ccUploaderURL = flag.String(
-	"ccUploaderURL",
-	"",
-	"URL of the cc uploader",
-)
-
-var dockerRegistryAddress = flag.String(
-	"dockerRegistryAddress",
-	"",
-	"Address (host:port) of the docker registry",
-)
-
-var consulCluster = flag.String(
-	"consulCluster",
-	"",
-	"Consul Agent URL",
-)
-
-var dockerStagingStack = flag.String(
-	"dockerStagingStack",
-	"",
-	"Stack to use for staging Docker applications",
-)
-
-var bbsCACert = flag.String(
-	"bbsCACert",
-	"",
-	"path to certificate authority cert used for mutually authenticated TLS BBS communication",
-)
-
-var bbsClientCert = flag.String(
-	"bbsClientCert",
-	"",
-	"path to client cert used for mutually authenticated TLS BBS communication",
-)
-
-var bbsClientKey = flag.String(
-	"bbsClientKey",
-	"",
-	"path to client key used for mutually authenticated TLS BBS communication",
-)
-
-var bbsClientSessionCacheSize = flag.Int(
-	"bbsClientSessionCacheSize",
-	0,
-	"Capacity of the ClientSessionCache option on the TLS configuration. If zero, golang's default will be used",
-)
-
-var bbsMaxIdleConnsPerHost = flag.Int(
-	"bbsMaxIdleConnsPerHost",
-	0,
-	"Controls the maximum number of idle (keep-alive) connctions per host. If zero, golang's default will be used",
+	"path to the stager configuration file",
 )
 
 var insecureDockerRegistries = make(vars.StringList)
@@ -151,35 +43,36 @@ const (
 )
 
 func main() {
-	debugserver.AddFlags(flag.CommandLine)
-	cflager.AddFlags(flag.CommandLine)
-
-	flag.Var(
-		&insecureDockerRegistries,
-		"insecureDockerRegistry",
-		"Docker registry to allow connecting to even if not secure. (Can be specified multiple times to allow insecure connection to multiple repositories)",
-	)
-
-	lifecycles := flags.LifecycleMap{}
-	flag.Var(&lifecycles, "lifecycle", "app lifecycle binary bundle mapping (lifecycle[/stack]:bundle-filepath-in-fileserver)")
 	flag.Parse()
+	logger := lager.NewLogger("stager")
 
-	logger, reconfigurableSink := cflager.New("stager")
-	initializeDropsonde(logger)
+	stagerConfig, err := config.NewStagerConfig(*configPath)
+	if err != nil {
+		logger.Fatal(fmt.Sprintf("failed to parse config file %q", *configPath), err)
+	}
+	lifecycles := flags.LifecycleMap{}
+	for _, value := range stagerConfig.Lifecycles {
+		if err := lifecycles.Set(value); err != nil {
+			logger.Fatal("failed-invalid-lifecycles", err)
+		}
+	}
 
-	ccClient := cc_client.NewCcClient(*ccBaseURL, *ccUsername, *ccPassword, *skipCertVerify)
+	reconfigurableSink := newReconfigurableSink(stagerConfig.LagerConfig.LogLevel)
+	logger.RegisterSink(reconfigurableSink)
+	initializeDropsonde(logger, stagerConfig)
 
-	backends := initializeBackends(logger, lifecycles)
+	ccClient := cc_client.NewCcClient(stagerConfig.CCBaseUrl, stagerConfig.CCUsername, stagerConfig.CCPassword, stagerConfig.SkipCertVerify)
+	backends := initializeBackends(logger, lifecycles, stagerConfig)
 
-	handler := handlers.New(logger, ccClient, initializeBBSClient(logger), backends, clock.NewClock())
+	handler := handlers.New(logger, ccClient, initializeBBSClient(logger, stagerConfig), backends, clock.NewClock())
 
 	clock := clock.NewClock()
-	consulClient, err := consuladapter.NewClientFromUrl(*consulCluster)
+	consulClient, err := consuladapter.NewClientFromUrl(stagerConfig.ConsulCluster)
 	if err != nil {
 		logger.Fatal("new-client-failed", err)
 	}
 
-	_, portString, err := net.SplitHostPort(*listenAddress)
+	_, portString, err := net.SplitHostPort(stagerConfig.ListenAddress)
 	if err != nil {
 		logger.Fatal("failed-invalid-listen-address", err)
 	}
@@ -191,7 +84,7 @@ func main() {
 	registrationRunner := initializeRegistrationRunner(logger, consulClient, portNum, clock)
 
 	members := grouper.Members{
-		{"server", http_server.New(*listenAddress, handler)},
+		{"server", http_server.New(stagerConfig.ListenAddress, handler)},
 		{"registration-runner", registrationRunner},
 	}
 
@@ -217,45 +110,45 @@ func main() {
 	logger.Info("stopped")
 }
 
-func initializeDropsonde(logger lager.Logger) {
-	dropsondeDestination := fmt.Sprint("localhost:", *dropsondePort)
+func initializeDropsonde(logger lager.Logger, stagerConfig config.StagerConfig) {
+	dropsondeDestination := fmt.Sprint("localhost:", stagerConfig.DropsondePort)
 	err := dropsonde.Initialize(dropsondeDestination, dropsondeOrigin)
 	if err != nil {
 		logger.Error("failed to initialize dropsonde: %v", err)
 	}
 }
 
-func initializeBackends(logger lager.Logger, lifecycles flags.LifecycleMap) map[string]backend.Backend {
-	_, err := url.Parse(*stagingTaskCallbackURL)
+func initializeBackends(logger lager.Logger, lifecycles flags.LifecycleMap, stagerConfig config.StagerConfig) map[string]backend.Backend {
+	_, err := url.Parse(stagerConfig.StagingTaskCallbackURL)
 	if err != nil {
 		logger.Fatal("Invalid staging task callback url", err)
 	}
-	if *dockerStagingStack == "" {
+	if stagerConfig.DockerStagingStack == "" {
 		logger.Fatal("Invalid Docker staging stack", errors.New("dockerStagingStack cannot be blank"))
 	}
 
-	_, err = url.Parse(*consulCluster)
+	_, err = url.Parse(stagerConfig.ConsulCluster)
 	if err != nil {
 		logger.Fatal("Error parsing consul agent URL", err)
 	}
-	_, err = url.Parse(*dockerRegistryAddress)
+	_, err = url.Parse(stagerConfig.DockerRegistryAddress)
 	if err != nil {
 		logger.Fatal("Error parsing Docker Registry address", err)
 	}
 
 	config := backend.Config{
 		TaskDomain:               cc_messages.StagingTaskDomain,
-		StagerURL:                *stagingTaskCallbackURL,
-		FileServerURL:            *fileServerURL,
-		CCUploaderURL:            *ccUploaderURL,
+		StagerURL:                stagerConfig.StagingTaskCallbackURL,
+		FileServerURL:            stagerConfig.FileServerUrl,
+		CCUploaderURL:            stagerConfig.CCUploaderURL,
 		Lifecycles:               lifecycles,
-		DockerRegistryAddress:    *dockerRegistryAddress,
+		DockerRegistryAddress:    stagerConfig.DockerRegistryAddress,
 		InsecureDockerRegistries: insecureDockerRegistries.Values(),
-		ConsulCluster:            *consulCluster,
-		SkipCertVerify:           *skipCertVerify,
-		PrivilegedContainers:     *privilegedContainers,
+		ConsulCluster:            stagerConfig.ConsulCluster,
+		SkipCertVerify:           stagerConfig.SkipCertVerify,
+		PrivilegedContainers:     stagerConfig.PrivilegedContainers,
 		Sanitizer:                backend.SanitizeErrorMessage,
-		DockerStagingStack:       *dockerStagingStack,
+		DockerStagingStack:       stagerConfig.DockerStagingStack,
 	}
 
 	return map[string]backend.Backend{
@@ -264,17 +157,17 @@ func initializeBackends(logger lager.Logger, lifecycles flags.LifecycleMap) map[
 	}
 }
 
-func initializeBBSClient(logger lager.Logger) bbs.Client {
-	bbsURL, err := url.Parse(*bbsAddress)
+func initializeBBSClient(logger lager.Logger, stagerConfig config.StagerConfig) bbs.Client {
+	bbsURL, err := url.Parse(stagerConfig.BBSAddress)
 	if err != nil {
 		logger.Fatal("Invalid BBS URL", err)
 	}
 
 	if bbsURL.Scheme != "https" {
-		return bbs.NewClient(*bbsAddress)
+		return bbs.NewClient(stagerConfig.BBSAddress)
 	}
 
-	bbsClient, err := bbs.NewSecureClient(*bbsAddress, *bbsCACert, *bbsClientCert, *bbsClientKey, *bbsClientSessionCacheSize, *bbsMaxIdleConnsPerHost)
+	bbsClient, err := bbs.NewSecureClient(stagerConfig.BBSAddress, stagerConfig.BBSCACert, stagerConfig.BBSClientCert, stagerConfig.BBSClientKey, stagerConfig.BBSClientSessionCacheSize, stagerConfig.BBSMaxIdleConnsPerHost)
 	if err != nil {
 		logger.Fatal("Failed to configure secure BBS client", err)
 	}
@@ -290,4 +183,21 @@ func initializeRegistrationRunner(logger lager.Logger, consulClient consuladapte
 		},
 	}
 	return locket.NewRegistrationRunner(logger, registration, consulClient, locket.RetryInterval, clock)
+}
+func newReconfigurableSink(logLevel string) *lager.ReconfigurableSink {
+	var minLagerLogLevel lager.LogLevel
+	switch logLevel {
+	case "debug":
+		minLagerLogLevel = lager.DEBUG
+	case "info":
+		minLagerLogLevel = lager.INFO
+	case "error":
+		minLagerLogLevel = lager.ERROR
+	case "fatal":
+		minLagerLogLevel = lager.FATAL
+	default:
+		panic(fmt.Errorf("unknown log level: %s", logLevel))
+	}
+
+	return lager.NewReconfigurableSink(lager.NewWriterSink(os.Stdout, lager.DEBUG), minLagerLogLevel)
 }
